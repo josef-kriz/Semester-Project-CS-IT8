12f09c8692f3178035a3e02b74987b3e5c9ca734
jediflamaster <jediflamaster.kg@gmail.com>
2019-04-04 13:43:59 +0200

    registering single misfires and dumping all in the same file

--- dumper/misfiredetector.py
+++ dumper/misfiredetector.py
@@ -2,41 +2,34 @@ from chpdb import cursor
 from model.misfirecluster import MisfireCluster
 import datetime
 import pickle
+
+
 cursor.execute('SELECT id FROM anlaeg')
 machineIDresults = cursor.fetchall()
+misfireClusters = []
 count = 0
-counts = [0, 0, 0, 0, 0]
 for machineIDresult in machineIDresults:
     count+=1
     print(str(count) + ' of ' + str(len(machineIDresults)))
     machineID = machineIDresult[0]
     cursor.execute('SELECT dato FROM anlaegshaendelser WHERE haendelse=112 AND anlaeg_id='+str(machineID) + ' ORDER BY dato')
     results = cursor.fetchall()
-    if len(results) < 2:
+    if len(results) < 1:
         continue
-    misfireClusters = []
-    iterator = results.__iter__()
-    lastMisfire = iterator.__next__()[0]
-    currentlyCluster = False
-    for result in iterator:
+    lastMisfire = datetime.datetime.min
+    
+    for result in results:
         date = result[0]
         if (date - lastMisfire) > datetime.timedelta(hours = 5):
-            currentlyCluster = False
             lastMisfire = date
+            misfireClusters.append(MisfireCluster(machineID =machineID, date = lastMisfire, duration = datetime.timedelta(seconds = 0), count = 1))
             continue
-        if currentlyCluster:
-            misfireClusters[-1].addMisfire(date)
         else:
-            misfireClusters.append(MisfireCluster(machineID =machineID, date = lastMisfire, duration = date - lastMisfire, count = 2))
-        currentlyCluster = True
-    if len(misfireClusters) == 0:
-        continue
-    for cluster in misfireClusters:
-        counts[int(cluster.duration / datetime.timedelta(hours = 1))] += 1
-    print(counts)
-    f = open('misfireClusters/' + str(machineID) + '_clusters.rick', 'wb')
-    pickle.dump(misfireClusters, f)
-    f.close()
+            misfireClusters[-1].addMisfire(date)    
+
+f = open('clusters.rick', 'wb')
+pickle.dump(misfireClusters, f)
+f.close()
 print(counts)
 
 
