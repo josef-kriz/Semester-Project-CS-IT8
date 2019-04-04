a120344a485f2dad1639c540e1fbe24191a25d81
Josef Kriz <email@josefkriz.com>
2019-04-04 12:02:32 +0200

    refactor

--- ecpower/machine_analyzer.py
+++ ecpower/machine_analyzer.py
@@ -5,13 +5,13 @@ import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 
 # machine_id = 1005171049
-# machine_id = 2100102907
-machine_id = 2000410280
+# machine_id = 2000410280
+machine_id = 1100405472
 db_row_limit = 1000  # limit for the number of rows being queried from the database
-interval = 'W'  # resampling time interval (handbook: http://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects)
+interval = 'H'  # resampling time interval (handbook: http://pandas.pydata.org/pandas-docs/stable/user_guide/timeseries.html#dateoffset-objects)
 cluster_threshold = 2  # number of misfires in the interval for them to be considered as a cluster
 
-# Define the MySQL connection
+# define the MySQL connection
 db = mysql.connector.connect(
     host="192.38.56.161",
     port="3306",
@@ -20,20 +20,20 @@ db = mysql.connector.connect(
     database="ecpower"
 )
 
-# Execute the query
-# Get all the misfire incidents for the given machine
+# execute the query
+# get all the misfire incidents for the given machine
 cursor = db.cursor()
 cursor.execute(f'SELECT * FROM anlaegshaendelser WHERE anlaeg_id = {machine_id} AND haendelse = 112 LIMIT {db_row_limit};')
 
-# Turn the result into a pandas DataFrame
+# turn the result into a pandas DataFrame
 result = pd.DataFrame(cursor.fetchall(), columns=['id', 'dato', 'effekt', 'haendelse', 'opkald_id', 'anlaeg_id', 'opkdato', 'sortorder', 'groupId', 'admin', 'timeZone'])
 
 print(f'Total of {len(result)} misfires for this machine found (limit {db_row_limit}).')
 
-# Create a new column with the date in correct format
+# create a new column with the date in correct format
 result['date'] = pd.to_datetime(result['dato'])
 
-# Create a new DataFrame using the new date column from result
+# create a new DataFrame using the new date column from result
 # put 1 for misfire count and default dot size of 50
 dates = result.loc[:, ['date']]
 dates['size'] = pd.Series(np.full((dates.shape[0],), 50, dtype=int))
@@ -42,13 +42,13 @@ dates['misfires'] = pd.Series(np.ones((dates.shape[0],), dtype=int))
 # resample the dates dataframe according to the given interval
 resampled = dates.resample(interval, on='date').sum()
 
-# get all rows with misfire count greater than cluster_threshold
-nonzero = resampled[(resampled.T != 0).any()]
-nonzero = nonzero.loc[nonzero['misfires'] >= cluster_threshold]
-
-print(f'Total of {len(nonzero)} clusters found where at least {cluster_threshold} happened in {interval}')
-
 register_matplotlib_converters()
 
+# draw the plot
 plt.scatter(resampled.index, resampled['misfires'], s=resampled['size'], c='red')
 plt.show()
+
+# get all rows with misfire count greater than cluster_threshold
+clusters = resampled.loc[resampled['misfires'] >= cluster_threshold]
+
+print(f'Total of {len(clusters)} clusters found where at least {cluster_threshold} happened in {interval}')
