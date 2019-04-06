c712e08fbbe251342d3fe6576e7193f6d1028599
Josef Kriz <email@josefkriz.com>
2019-04-06 19:40:05 +0200

    parse sensor values

--- ecpower/machine_analyzer.py
+++ ecpower/machine_analyzer.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import pandas as pd
 import numpy as np
+from dumper.src.parser import parse_value
 import matplotlib.pyplot as plt
 from pandas.plotting import register_matplotlib_converters
 import os
@@ -28,10 +29,13 @@ cursor = db.cursor()
 def get_misfires(db_cursor, machine, row_limit=1000):
     # execute the query
     # get all the misfire incidents for the given machine
-    db_cursor.execute(f'SELECT * FROM anlaegshaendelser WHERE anlaeg_id = {machine} AND haendelse = 112 LIMIT {row_limit};')
+    db_cursor.execute(
+        f'SELECT * FROM anlaegshaendelser WHERE anlaeg_id = {machine} AND haendelse = 112 LIMIT {row_limit};')
 
     # turn the result into a pandas DataFrame
-    return pd.DataFrame(db_cursor.fetchall(), columns=['id', 'dato', 'effekt', 'haendelse', 'opkald_id', 'anlaeg_id', 'opkdato', 'sortorder', 'groupId', 'admin', 'timeZone'])
+    return pd.DataFrame(db_cursor.fetchall(),
+                        columns=['id', 'dato', 'effekt', 'haendelse', 'opkald_id', 'anlaeg_id', 'opkdato', 'sortorder',
+                                 'groupId', 'admin', 'timeZone'])
 
 
 # resample the data to smaller chunks and sum them up
@@ -56,7 +60,41 @@ def get_calls(db_cursor, machine, row_limit=10000):
     db_cursor.execute(f'SELECT * FROM opkald2 WHERE anlaegId = {machine} LIMIT {row_limit};')
 
     # turn the result into a pandas DataFrame
-    return pd.DataFrame(db_cursor.fetchall(), columns=['id', 'dateCreated', 'opkdato', 'anlaegId', 'kaldsgrund', 'driftsform', 'tankfoeler1', 'tankfoeler2', 'tankfoeler3', 'kogealarm', 'olietryk', 'vandalarm', 'ekstraalarm', 'driftsminutter', 'husets_elforbrug', 'anlaeggets_elproduktion', 'total_driftstid', 'antal_starter', 'total_stilstandstid', 'varmepumpe1_indkoblet', 'varmepumpe1_starter', 'varmepumpe2_indkoblet', 'varmepumpe2_starter', 'lavt_forbrug', 'underskud', 'totalt_underskud', 'tab', 'gasalarm', 'fasefejl', 'rumtemp', 'antennesignal', 'stopminutter', 'motorvaern', 'el_solgt', 'varmeproduktion', 'braendselsforbrug', 'olieskift_ns', 'olieskift_nl', 'olieskift_nc', 'olieskift_nh', 'fly_start', 'fly_hot', 'fly_trailing_hot', 'fly_cool', 'fly_coefficient', 'storage_fill', 'fd', 'af', 'um', 'debug_info', 'brandalarm', 'odd_storage', 'varmefordeler_printtemparatur', 'mv', 'mk', 'lw', 'lk', 'vr', 'varmefordeler_transport', 'lager_procent', 'sa', 'sb', 'ht', 'hb', 'flow_control_setpoint', 'flow_control_fremloebs', 'flow_control_retur', 'av', 'st', 'ps', 'fl', 'ev', 'fv', 'mp', 'hp', 'fm', 'fp', 'gw', 'gs', 'gt', 'cr', 'gassikker_printtemp', 'flowcontrolusersetpoint', 'lr', 'datasampling', 'fejlkontrol', 'fejlposition', 'aa', 'ab', 'ac', 'ad', 'ae', 'ag', 'ah', 'ai', 'aj', 'ak', 'am', 'an', 'ao', 'uid', 'stempel', 'rs', 'cv_psu_voltage', 'last_rejected_start_reason', 'wf_high_return_temp', 'ecu_pcb_temp', 'ecu_vandtemp', 'ecu_psu_voltage', 'misfire_1', 'misfire_2', 'injector_current', 'ignitor_current', 'nb_current', 'start_map_pressure', 'power_step_jitter', 'wot_map_pressure', 'wct_map_pressure', 'actual_map_pressure', 'actual_powerstep_position', 'actual_ventil_position', 'actual_injector_time', 'actual_rpm', 'requested_power', 'aim_power', 'lambda_setpoint', 'lambda_voltage', 'map_offset', 'ignition_angel', 'dwell_time', 'aktuel_elproduktion', 'load_level', 'loadsharer_totalconsumption', 'misfire_leds', 'misfire_log', 'lgvgp', 'bl_engineval', 'bq_flowmastervalveposition', 'bm_flowmastersrctemp', 'bn_flowmasterbypasstemp', 'bp_flowmasterpsuvoltage', 'bo_flowmasterpcbtemp', 'br_aktuelelproduktion', 'bs_antalgeneratorstart', 'bx_nspfase1', 'by_nspfase2', 'bz_nspfase3', 'ca_netfrekvens', 'reginaInstance', 'cjManualStatus', 'ckAuxTempTrackerSensor1', 'clAuxTempTrackerSensor2', 'cmAuxTempTrackerSensor3', 'cnAuxTempTrackerSensor4', 'CQVFMasterSourceTempActive', 'CSSWValidationResult', 'DINumberOfErrorCalls', 'DKBoilerReleaseStatus', 'DOSurgeProtecterStatus', 'DUHPC1ReleaseTime', 'GJHPC1ReleaseStatus', 'GEPMH1ReleaseTime', 'GFPMH1OperationalTime', 'GOPMH2OperationalTime', 'GGPMH1HpvTemp', 'GPPMH2HpvTemp', 'GHPMH1HpkTemp', 'GQPMH2HpkTemp', 'GIPMH1ReleaseStatus', 'GRPMH2ReleaseStatus'])
+    return pd.DataFrame(db_cursor.fetchall(),
+                        columns=['id', 'dateCreated', 'opkdato', 'anlaegId', 'kaldsgrund', 'driftsform', 'tankfoeler1',
+                                 'tankfoeler2', 'tankfoeler3', 'kogealarm', 'olietryk', 'vandalarm', 'ekstraalarm',
+                                 'driftsminutter', 'husets_elforbrug', 'anlaeggets_elproduktion', 'total_driftstid',
+                                 'antal_starter', 'total_stilstandstid', 'varmepumpe1_indkoblet', 'varmepumpe1_starter',
+                                 'varmepumpe2_indkoblet', 'varmepumpe2_starter', 'lavt_forbrug', 'underskud',
+                                 'totalt_underskud', 'tab', 'gasalarm', 'fasefejl', 'rumtemp', 'antennesignal',
+                                 'stopminutter', 'motorvaern', 'el_solgt', 'varmeproduktion', 'braendselsforbrug',
+                                 'olieskift_ns', 'olieskift_nl', 'olieskift_nc', 'olieskift_nh', 'fly_start', 'fly_hot',
+                                 'fly_trailing_hot', 'fly_cool', 'fly_coefficient', 'storage_fill', 'fd', 'af', 'um',
+                                 'debug_info', 'brandalarm', 'odd_storage', 'varmefordeler_printtemparatur', 'mv', 'mk',
+                                 'lw', 'lk', 'vr', 'varmefordeler_transport', 'lager_procent', 'sa', 'sb', 'ht', 'hb',
+                                 'flow_control_setpoint', 'flow_control_fremloebs', 'flow_control_retur', 'av', 'st',
+                                 'ps', 'fl', 'ev', 'fv', 'mp', 'hp', 'fm', 'fp', 'gw', 'gs', 'gt', 'cr',
+                                 'gassikker_printtemp', 'flowcontrolusersetpoint', 'lr', 'datasampling', 'fejlkontrol',
+                                 'fejlposition', 'aa', 'ab', 'ac', 'ad', 'ae', 'ag', 'ah', 'ai', 'aj', 'ak', 'am', 'an',
+                                 'ao', 'uid', 'stempel', 'rs', 'cv_psu_voltage', 'last_rejected_start_reason',
+                                 'wf_high_return_temp', 'ecu_pcb_temp', 'ecu_vandtemp', 'ecu_psu_voltage', 'misfire_1',
+                                 'misfire_2', 'injector_current', 'ignitor_current', 'nb_current', 'start_map_pressure',
+                                 'power_step_jitter', 'wot_map_pressure', 'wct_map_pressure', 'actual_map_pressure',
+                                 'actual_powerstep_position', 'actual_ventil_position', 'actual_injector_time',
+                                 'actual_rpm', 'requested_power', 'aim_power', 'lambda_setpoint', 'lambda_voltage',
+                                 'map_offset', 'ignition_angel', 'dwell_time', 'aktuel_elproduktion', 'load_level',
+                                 'loadsharer_totalconsumption', 'misfire_leds', 'misfire_log', 'lgvgp', 'bl_engineval',
+                                 'bq_flowmastervalveposition', 'bm_flowmastersrctemp', 'bn_flowmasterbypasstemp',
+                                 'bp_flowmasterpsuvoltage', 'bo_flowmasterpcbtemp', 'br_aktuelelproduktion',
+                                 'bs_antalgeneratorstart', 'bx_nspfase1', 'by_nspfase2', 'bz_nspfase3',
+                                 'ca_netfrekvens', 'reginaInstance', 'cjManualStatus', 'ckAuxTempTrackerSensor1',
+                                 'clAuxTempTrackerSensor2', 'cmAuxTempTrackerSensor3', 'cnAuxTempTrackerSensor4',
+                                 'CQVFMasterSourceTempActive', 'CSSWValidationResult', 'DINumberOfErrorCalls',
+                                 'DKBoilerReleaseStatus', 'DOSurgeProtecterStatus', 'DUHPC1ReleaseTime',
+                                 'GJHPC1ReleaseStatus', 'GEPMH1ReleaseTime', 'GFPMH1OperationalTime',
+                                 'GOPMH2OperationalTime', 'GGPMH1HpvTemp', 'GPPMH2HpvTemp', 'GHPMH1HpkTemp',
+                                 'GQPMH2HpkTemp', 'GIPMH1ReleaseStatus', 'GRPMH2ReleaseStatus']).set_index(
+        'id').replace({pd.np.nan: None})
 
 
 # draw and save the plots
@@ -93,7 +131,11 @@ calls = get_calls(cursor, machine_id, db_row_limit)
 
 print(f'\t{len(calls)} calls found')
 
-# print(calls)
+# parse all sensor values
+for column in calls:
+    calls[column] = calls[column].apply(lambda x: parse_value(column, x))
+
+# print(calls.head())
 
 draw_clusters(resampled, interval, output_path)
 print(f'\tplots saved to {output_path}')
