986dd801b0f40530440da45c462b6f7352e88208
jediflamaster <jediflamaster.kg@gmail.com>
2019-04-07 14:38:59 +0200

    Separated single misfires

--- dumper/misfiregrouper.py
+++ dumper/misfiregrouper.py
@@ -11,9 +11,12 @@ try:
     print(sys.argv[2], ' seconds')
 except: print("First argument must be path to the file with misfire cluster data, second argument must be grouping time interval in seconds")
 
-output = []
+output = [[]]
 for cluster in data:
-    interval = int(cluster.duration / timedelta(seconds = int(sys.argv[2])))
+    if cluster.count == 1:
+        output[0].append(cluster)
+        continue
+    interval = int(cluster.duration / timedelta(seconds = int(sys.argv[2]))) + 1
     while interval >= len(output):
         output.append([])
     output[interval].append(cluster) 
