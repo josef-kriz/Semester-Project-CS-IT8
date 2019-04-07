fa8ed47b6ff38604236a66c2681f52446ee6eede
jediflamaster <jediflamaster.kg@gmail.com>
2019-04-07 17:35:25 +0200

    grouper outputs counts of groups to console

--- dumper/misfiregrouper.py
+++ dumper/misfiregrouper.py
@@ -4,11 +4,10 @@ import pickle
 import sys
 
 data =[]
-print(sys.argv)
-
+intervalDuration = timedelta()
 try:
+    intervalDuration = timedelta(seconds = int(sys.argv[2]))
     data = pickle.load(open(sys.argv[1], "rb"))
-    print(sys.argv[2], ' seconds')
 except: print("First argument must be path to the file with misfire cluster data, second argument must be grouping time interval in seconds")
 
 output = [[]]
@@ -16,11 +15,15 @@ for cluster in data:
     if cluster.count == 1:
         output[0].append(cluster)
         continue
-    interval = int(cluster.duration / timedelta(seconds = int(sys.argv[2]))) + 1
+    interval = int(cluster.duration / intervalDuration) + 1
     while interval >= len(output):
         output.append([])
     output[interval].append(cluster) 
-print(output)
+print('Singles:', len(output[0]) )
+i = 1
+while i<len(output):
+    print(str((i-1)*intervalDuration), 'to', str(i* intervalDuration), ':', len(output[i]))
+    i+=1
 f = open('clustersgrouped.rick', 'wb')
 pickle.dump(output, f)
 f.close()
