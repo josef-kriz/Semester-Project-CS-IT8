74480eadfe95701167895f33b5c65e05780f7196
jediflamaster <jediflamaster.kg@gmail.com>
2019-05-01 10:16:18 +0200

    added detection of misfire shutdowns

--- incidents-analysis/misfiredata.py
+++ incidents-analysis/misfiredata.py
@@ -7,10 +7,10 @@ sensornames = ['actual_rpm']
 incidents = [103, 101, 100, 102]
 
 # queries the database for preceding incidents
-def get_cluster_incidents(cursor, cluster, interval, haendelser = []):
-    start = cluster.date - datetime.timedelta(seconds=interval)
+def get_cluster_incidents(date, machineID, interval, haendelser = []):
+    start = date - datetime.timedelta(seconds=interval)
     query = "SELECT haendelse, COUNT(*) FROM anlaegshaendelser " \
-            " WHERE anlaeg_id = " + str(cluster.machineID) + " AND dato < \"" + str(cluster.date) + "\" AND " \
+            " WHERE anlaeg_id = " + str(machineID) + " AND dato < \"" + str(date) + "\" AND " \
             " dato > \"" + str(start) + "\""
     if haendelser != []:
             query += " AND haendelse IN (" + str(haendelser[0])
@@ -23,17 +23,21 @@ def get_cluster_incidents(cursor, cluster, interval, haendelser = []):
     return cursor.fetchall()
 
 # queries the database for preceding sensor readings
-def get_cluster_sensors(cursor, cluster, interval, sensors):
-    start = cluster.date - datetime.timedelta(seconds=interval)
+def get_cluster_sensors(date, machineID, interval, sensors):
+    start = date - datetime.timedelta(seconds=interval)
     query = "SELECT " + sensors[0] 
     for i in range(1, len(sensors)):
         query += ", " + sensors[i]
-    query += " FROM opkald2 WHERE anlaegid = " + str(cluster.machineID) + " AND opkdato > \"" + str(start) + "\" AND opkdato < \"" + str(cluster.date) + "\" "
+    query += " FROM opkald2 WHERE anlaegid = " + str(machineID) + " AND opkdato > \"" + str(start) + "\" AND opkdato < \"" + str(date) + "\" "
     query += " ORDER BY anlaegid;"
     print(query)
     cursor.execute(query)
     return cursor.fetchall()
 
+#Returns true if there is a misfire
+def check_for_future_misfire(date, machineID, interval):
+    cursor.execute("SELECT opkdato FROM anlaegshaendelser WHERE haendelse = 112 AND misfire_shutdown = 1 AND opkdato > \"" + str(date) + "\" AND opkdato < \"" + str(date + interval) "\" ")
+    return cursor.fetchall() != []
     
 def parse_sensors(sensordata, sensornames):
     values = []
@@ -43,9 +47,3 @@ def parse_sensors(sensordata, sensornames):
             i += 1
     return values
 
-data = pickle.load(open("clusters.rick", "rb"))
-sensordata = get_cluster_sensors(cursor, data[0], 1209600, sensornames)
-print(sensordata)
-for row in sensordata:
-    print(parse_sensors(row, sensornames))
-print(get_cluster_incidents(cursor, data[0], 1209600))
