ccfb20c634ae2d64e5f3963816c7e5314ed9a2a9
Honza Kopecký <honza.kopecky95@gmail.com>
2019-05-01 15:58:25 +0200

    Add comments

--- training-data/src/sensor_data.py
+++ training-data/src/sensor_data.py
@@ -25,12 +25,12 @@ def get_sensor_data(date, machineID, sensors, count):
     return dates, sensors
 
 
+# iterates over the database values and puts the values in an array
 def parse_sensors(sensor_rows, sensor_names):
     values = []
     i = 0
     # iterate over all sensor names to be produced
     for sensor_name in sensor_names:
-        sensor_values = []
         # iterate over all opkald2 messages returned
         for row in sensor_rows:
             # parse the value according to the sensor name
@@ -42,6 +42,7 @@ def parse_sensors(sensor_rows, sensor_names):
     return values
 
 
+# compute time intervals between opkald dates and current reference datetime
 def parse_dates(dates, reference):
     intervals = []
     for i in range(0, len(dates)):
@@ -51,12 +52,12 @@ def parse_dates(dates, reference):
     return intervals
 
 
+# function retrieves specified number of sensor readings prior to a specified time
 def fetch_sensors(datetime, machine_id, sensor_names, sensor_readings_count):
     res = []
     dates, sensor_readings = get_sensor_data(datetime, machine_id, sensor_names, sensor_readings_count)
     res.extend(parse_sensors(sensor_readings, sensor_names))
     res.extend(parse_dates(dates, datetime))
-    print(res)
     return res
 
 
