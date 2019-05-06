11c71f16dd50433e4e36ebf4da4021a9f68d6a03
Honza Kopecký <honza.kopecky95@gmail.com>
2019-05-06 15:57:23 +0200

    Add incidents & sensors of interest Add check to sample only the machines that experience misfire shutdown during smapling period Use pre fetched machine ids Skip the sample if one of the fetchers fails Print progress of the fetching Remove model related code

--- training-data/training_data.py
+++ training-data/training_data.py
@@ -1,4 +1,5 @@
 import datetime
+import pickle
 from src.chpdb import cursor
 from src.incidents_data import fetch_incidents
 from src.sensor_data import fetch_sensors
@@ -7,23 +8,44 @@ from src.sw_version import get_sw_version
 from src.past_misfires import get_past_misfires
 from src.machine_type import get_machine_type
 from src.target import get_target
-from sklearn import tree
+from data.misfire_shutdown_machines import training_machine_ids
+from random import shuffle
 # import graphviz
 
 settings = {
-    'incidents': [78, 4, 112],
+    'machines_count': 100,
+    'incidents': [1, 5, 6, 7, 10, 19, 20, 21, 49, 61, 62, 63, 64, 65, 66, 68, 76,
+                  79, 80, 81, 82, 83, 89, 100, 102, 103, 112, 113, 123, 130, 133],
     'incidents_past_interval': datetime.timedelta(days=7),
-    'sensors': ['actual_rpm', 'storage_fill'],
+    'sensors': ['aktuel_elproduktion','ab','ac','actual_map_pressure','actual_powerstep_position','actual_rpm',
+                'ak','anlaeggets_elproduktion','av','cv_psu_voltage','ecu_pcb_temp','ecu_vandtemp','flow_control_fremloebs',
+                'flow_control_retur','flow_control_setpoint','lk','lw','mk','mv','stopminutter',
+                'varmefordeler_printtemparatur'],
     'sensors_samples': 5,
     'misfire_interval': datetime.timedelta(days=1),
-    'sampling_interval': datetime.timedelta(days=1)
+    'sampling_interval': datetime.timedelta(days=1),
+    'training_output': 'output/training.pickle',
+    'test_output': 'output/test.pickle'
 }
 
-def get_machines(count):
-    cursor.execute("SELECT id "
-                   "FROM anlaeg "
-                   "ORDER BY RAND();")
-    return [x[0] for x in cursor.fetchall()]
+
+def get_machines(count=1000):
+    shuffle(training_machine_ids)
+    if count > len(training_machine_ids):
+        return training_machine_ids
+    else:
+        return training_machine_ids[:count]
+
+
+def is_machine_valid(machine_id, start, end):
+    query = "SELECT COUNT(1) " \
+            "FROM anlaegshaendelser " \
+            "WHERE anlaeg_id = {} " \
+            "AND dato >= \"{}\" " \
+            "AND dato <= \"{}\" " \
+            "AND misfire_shutdown = 1;".format(machine_id, start, end)
+    cursor.execute(query)
+    return cursor.fetchone()[0] >= 1
 
 
 def sample_machine(machine_id, ref_settings):
@@ -33,19 +55,32 @@ def sample_machine(machine_id, ref_settings):
         return False, False
 
     end_date = get_machine_end_date(machine_id)
+
+    if not is_machine_valid(machine_id, start_date, end_date):
+        print("\tSampling cancelled. No misfire shutdowns found in sampling period.")
+        return False, False
+
+    print("\tSampling from", start_date, "to", end_date)
+
     current_date = start_date
-    # print(start_date)
-    # print(end_date)
-    # print(current_date)
+
     samples_data = []
     samples_targets = []
     while current_date < end_date:
-        step_data, step_target = sampling_step(machine_id, current_date, ref_settings)
-        samples_data.append(step_data)
-        samples_targets.append(step_target)
+        try:
+            step_data, step_target = sampling_step(machine_id, current_date, ref_settings)
+            samples_data.append(step_data)
+            samples_targets.append(step_target)
+        except Exception as e:
+            str(e)
+            # print("Sample skipped.", str(e))
+
         current_date += ref_settings['sampling_interval']
-        if len(samples_data) > 20:
-            break
+
+    print("\tCreated", len(samples_targets), "samples.", len([x for x in samples_targets if x == 1]), "of them have misfire kills.")
+    feature_lengths = [len(x) for x in samples_data]
+    if len(feature_lengths) != 0:
+        print("\tAverage features array length is", sum(feature_lengths) / len(feature_lengths))
     return samples_data, samples_targets
 
 
@@ -126,61 +161,34 @@ def sampling_step(machine_id, ref_datetime, ref_settings):
     step_data.extend(get_machine_type(machine_id))
 
     step_target = get_target(ref_datetime, machine_id)
-    print(step_data, step_target)
+    # print(step_data, step_target)
     return step_data, step_target
 
 
-fetch_machines = get_machines(3)
-machines = fetch_machines[:2]
-test_machines = fetch_machines[2:4]
+def sample_machines_set(machine_ids):
+    features = []
+    targets = []
+    for index, id in enumerate(machine_ids):
+        print("#" + str(id) + " - " + str(index + 1) + " of " + str(len(machine_ids)))
+        machine_features, machine_targets = sample_machine(id, settings)
+        if machine_features and machine_targets:
+            features.extend(machine_features)
+            targets.extend(machine_targets)
+    return [features, targets]
 
-# machines = [2100780010]
-# test_machines = [2100780010]
 
-data = []
-targets = []
-test_data = []
-test_targets = []
+training_file = open(settings['training_output'], "wb")
+test_file = open(settings['test_output'], "wb")
 
-for machine in machines:
-    machine_samples, machine_targets = sample_machine(machine, settings)
+machines = get_machines(settings['machines_count'])
+split_index = int(len(machines) * 0.7)
 
-    if machine_samples and machine_targets:
-        data.extend(machine_samples)
-        targets.extend(machine_targets)
+training_machines = machines[:split_index]
+test_machines = machines[split_index:]
 
-for machine in test_machines:
-    test_samples, test_targets = sample_machine(machine, settings)
-
-    if test_samples and test_targets:
-        test_data.extend(test_samples)
-        test_targets.extend(test_targets)
-
-print(len(data), len(targets))
-
-clf = tree.DecisionTreeClassifier()
-clf = clf.fit(data, targets)
-
-test_sample1 = [0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 23102330.0, 22552315.0, 22552315.0, 21552215.0, 21552215.0, 4003.0, 47290.0, 90567.0, 133847.0, 177125.0, 56684, '0010140002', 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-test_sample2 = [7, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 22252245.0, 22102230.0, 22102230.0, 22252245.0, 22252245.0, 24880.0, 72306.0, 101180.0, 135341.0, 165746.0, 56684, '0010120003', 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-test_sample3 = [0, 0, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 23102330.0, 23102330.0, 21252145.0, 21252145.0, 21252145.0, 34777.0, 63659.0, 117798.0, 148916.0, 180110.0, 56684, '0010120003', 0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-
-predictions = clf.predict(test_data)
-
-correct_predictions = 0
-
-for i in range(0, len(predictions)):
-    if predictions[i] == test_targets[i]:
-        correct_predictions += 1
-
-print((correct_predictions / len(predictions)) * 100 + "%")
-
-# dot_data = tree.export_graphviz(clf, out_file=None,
-#                      # feature_names=iris.feature_names,
-#                      # class_names=iris.target_names,
-#                      filled=True, rounded=True,
-#                      special_characters=True)
-# graph = graphviz.Source(dot_data)
-# graph.render("iris")
+print("Training machines:")
+pickle.dump(sample_machines_set(training_machines), training_file)
+print("Test machines:")
+pickle.dump(sample_machines_set(test_machines), test_file)
 
 cursor.close()
